12912b4ef5a76229af48fa34eeb0f50bdf141acc
Pavin7 <pavinwere10@gmail.com>
2023-07-27 10:54:46 +0300

    first commit

---
+++ samdjangoproject/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render, redirect
+from .models import People
+
+
+# function for index page
+
+def indexpage(request):
+    data = People.objects.all()
+    context = {"data": data}
+    return render(request, "index.html", context)
+
+
+# Function to delete data
+def deleteData(request, id):
+    d = People.objects.get(id=id)
+    d.delete()
+    return redirect("/")
+    return render(request, "index.html")
+
+
+# Function to update our records
+def updateData(request, id):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        age = request.POST.get('age')
+        gender = request.POST.get('gender')
+
+        edit_data = People.objects.get(id=id)
+        edit_data.name = name
+        edit_data.email = email
+        edit_data.age = age
+        edit_data.gender = gender
+        edit_data.save()
+
+        return redirect("/")
+
+    dta = People.objects.get(id=id)
+    context = {"dta": dta}
+    return render(request, "edit.html", context)
+
+
+def insertData(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        age = request.POST.get('age')
+        gender = request.POST.get('gender')
+
+        query = People.objects.create(name=name, email=email, age=age, gender=gender)
+        query.save()
+        return redirect("/")
+    return render(request, "index.html")
